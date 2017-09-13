1dc225c6deeeb6715f6ed76f3a4adc5d0871f418
Pavel Milkevich <pavel.milkevich@gmail.com>
2017-09-13 12:13:19 +0300

    Move LiveEdit's code and styles and rename it to Page Editor #4

--- modules/app-contentstudio/webpack.config.js
+++ modules/app-contentstudio/webpack.config.js
@@ -1,20 +1,41 @@
 const RelativeErrorsWebpackPlugin = require('./util/relativeErrorsWebpackPlugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: './src/main/resources/assets/js/main.ts',
+    context: __dirname + '/src/main/resources',
+    entry: {
+        bundle: './assets/js/main.ts',
+        'page-editor': './page-editor/js/main.ts'
+    },
     output: {
-        filename: './build/resources/main/assets/js/bundle.js'
+        path: __dirname + '/build/resources/main/',
+        filename: './assets/js/[name].js'
     },
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js', '.less', '.css']
     },
     module: {
         loaders: [
-            {test: /\.tsx?$/, loader: "ts-loader"}
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader"
+            },
+            {
+                test: /\.less$/,
+                // use: ExtractTextPlugin.extract({
+                use: ['css-loader', 'less-loader']//,
+                //fallback: 'style-loader' // use style-loader in development
+                // })
+            }
         ]
     },
     plugins: [
-        RelativeErrorsWebpackPlugin
+        RelativeErrorsWebpackPlugin//,
+        /*        new ExtractTextPlugin({
+                    filename: "./page-editor/styles/page-editor_all.css",
+                    allChunks: true,
+                    disable: false //process.env.NODE_ENV === "development"
+                })*/
     ],
     devtool: 'source-map'
 };
