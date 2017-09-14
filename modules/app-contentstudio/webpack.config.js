90be2017a20069cc78372247ee1bdec07c8592e3
Pavel Milkevich <pavel.milkevich@gmail.com>
2017-09-14 22:15:53 +0300

    Move LiveEdit's code and styles and rename it to Page Editor #4

--- modules/app-contentstudio/webpack.config.js
+++ modules/app-contentstudio/webpack.config.js
@@ -1,15 +1,20 @@
 const RelativeErrorsWebpackPlugin = require('./util/relativeErrorsWebpackPlugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
+const extractText = new ExtractTextPlugin({
+    filename: './styles/_all.css',
+    allChunks: true,
+    disable: false  //process.env.NODE_ENV === "development"
+});
+
 module.exports = {
-    context: __dirname + '/src/main/resources',
+    context: __dirname + '/src/main/resources/assets',
     entry: {
-        bundle: './assets/js/main.ts',
-        'page-editor': './page-editor/js/main.ts'
+        bundle: './js/main.ts'
     },
     output: {
-        path: __dirname + '/build/resources/main/',
-        filename: './assets/js/[name].js'
+        path: __dirname + '/build/resources/main/assets',
+        filename: './js/[name].js'
     },
     resolve: {
         extensions: ['.ts', '.js', '.less', '.css']
@@ -22,20 +27,24 @@ module.exports = {
             },
             {
                 test: /\.less$/,
-                use: ExtractTextPlugin.extract({
+                use: extractText.extract({
                     fallback: 'style-loader',
                     use: 'css-loader?importLoaders=1!less-loader'
                 })
-            }
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf)$/,
+                use: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: 'file-loader?name=img/[name].[ext]'
+            },
         ]
     },
     plugins: [
         RelativeErrorsWebpackPlugin,
-        new ExtractTextPlugin({
-            filename: './page-editor/styles/page-editor_all.css',
-            allChunks: true,
-            disable: false
-        })
+        extractText
     ],
     devtool: 'source-map'
 };
