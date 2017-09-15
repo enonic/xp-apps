38e8caee2e333a5cdf9ae4761c71a1895c180c9c
Taukachou Mikita <edloidas@gmail.com>
2017-09-15 17:35:18 +0300

    Set up linter task in gulp #11
    Added Autoprefixer and other optimizers from PostCSS.
    Removed unused Gulp config.

--- modules/app-main/webpack.config.js
+++ modules/app-main/webpack.config.js
@@ -2,14 +2,14 @@ const ErrorLoggerPlugin = require('error-logger-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    context: `${__dirname}/src/main/resources`,
+    context: `${__dirname}/src/main/resources/assets`,
     entry: {
-        home: './assets/js/home/main.js',
-        launcher: './assets/js/launcher/main.js',
+        home: './js/home/main.js',
+        launcher: './js/launcher/main.js',
     },
     output: {
-        path: `${__dirname}/build/resources/main/`,
-        filename: './assets/js/[name]/bundle.js'
+        path: `${__dirname}/build/resources/main/assets/`,
+        filename: './js/[name]/bundle.js'
     },
     module: {
         rules: [
@@ -17,23 +17,27 @@ module.exports = {
                 test: /\.less$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: 'css-loader!less-loader'
+                    use: [
+                        { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+                        { loader: 'postcss-loader', options: { sourceMap: true } },
+                        { loader: 'less-loader', options: { sourceMap: true } }
+                    ]
                 })
             },
             {
                 test: /\.(woff|woff2|eot|ttf|svg)$/,
-                loader: 'file-loader?name=./assets/icons/fonts/[name].[ext]',
+                loader: 'file-loader?name=./icons/fonts/[name].[ext]',
             },
             {
                 test: /\.(png|jpg|gif)$/,
-                loader: 'file-loader?name=./assets/icons/favicons/[name].[ext]',
+                loader: 'file-loader?name=./icons/favicons/[name].[ext]',
             }
         ]
     },
     plugins: [
         new ErrorLoggerPlugin(),
         new ExtractTextPlugin({
-            filename: './assets/styles/[name].css',
+            filename: './styles/[name].css',
             allChunks: true,
             disable: false
         })
