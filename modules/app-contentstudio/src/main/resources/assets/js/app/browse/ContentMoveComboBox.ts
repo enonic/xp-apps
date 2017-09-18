e595dacad38b9371395e121420530d6b5d4750cd
vbradnitski <vbr@enonic.com>
2017-09-18 17:56:38 +0300

    Change OptionDataLoader to derive from BaseLoader #5576

--- modules/app-contentstudio/src/main/resources/assets/js/app/browse/ContentMoveComboBox.ts
+++ modules/app-contentstudio/src/main/resources/assets/js/app/browse/ContentMoveComboBox.ts
@@ -1,21 +1,17 @@
 import '../../api.ts';
 
-import SelectedOption = api.ui.selector.combobox.SelectedOption;
-import MoveContentSummaryLoader = api.content.resource.MoveContentSummaryLoader;
 import ContentSummary = api.content.ContentSummary;
 import ContentSelectedOptionsView = api.content.ContentSelectedOptionsView;
 import ContentPath = api.content.ContentPath;
 import SelectedOptionsView = api.ui.selector.combobox.SelectedOptionsView;
-import RichComboBoxBuilder = api.ui.selector.combobox.RichComboBoxBuilder;
 import ContentTypeName = api.schema.content.ContentTypeName;
 import ContentComboBox = api.content.ContentComboBox;
 import ContentComboBoxBuilder = api.content.ContentComboBoxBuilder;
-import ContentSummaryOptionDataHelper = api.content.ContentSummaryOptionDataHelper;
 import ContentSummaryOptionDataLoader = api.content.ContentSummaryOptionDataLoader;
-import RichComboBox = api.ui.selector.combobox.RichComboBox;
 import ContentAndStatusTreeSelectorItem = api.content.resource.ContentAndStatusTreeSelectorItem;
+import ContentTreeSelectorItem = api.content.resource.ContentTreeSelectorItem;
 
-export class ContentMoveComboBox extends RichComboBox<any> {
+export class ContentMoveComboBox extends ContentComboBox {
 
     private readonlyChecker: MoveReadOnlyChecker;
 
@@ -25,40 +21,31 @@ export class ContentMoveComboBox extends RichComboBox<any> {
         richComboBoxBuilder
             .setMaximumOccurrences(1)
             .setComboBoxName('contentSelector')
-            .setLoader(new MoveContentSummaryLoader())
-            .setSelectedOptionsView(<SelectedOptionsView<ContentSummary>>new ContentSelectedOptionsView())
+            .setLoader(new ContentSummaryOptionDataLoader())
+            .setSelectedOptionsView(<SelectedOptionsView<ContentTreeSelectorItem>>new ContentSelectedOptionsView())
             .setOptionDisplayValueViewer(new api.content.ContentSummaryViewer())
             .setDelayedInputValueChangedHandling(500)
             .setSkipAutoDropShowOnValueChange(true)
             .setTreegridDropdownEnabled(true)
-            .setOptionDataHelper(new ContentSummaryOptionDataHelper())
-            .setOptionDataLoader(new ContentSummaryOptionDataLoader());
 
         super(richComboBoxBuilder);
         this.readonlyChecker = new MoveReadOnlyChecker();
+        
         this.getComboBox().getComboBoxDropdownGrid().setReadonlyChecker(this.readonlyChecker.isReadOnly.bind(this.readonlyChecker));
+        this.optionsFactory.setReadonlyChecker(this.readonlyChecker.isReadOnly.bind(this.readonlyChecker));
+        
         this.onOptionDeselected(() => {
             this.getComboBox().getInput().reset();
         });
     }
 
-    getLoader(): MoveContentSummaryLoader {
-        return <MoveContentSummaryLoader> this.loader;
-    }
-
     setFilterContents(contents: ContentSummary[]) {
-        this.getLoader().setFilterContentPaths(contents.map((content) => content.getPath()));
         this.readonlyChecker.setFilterContentPaths(contents.map((content) => content.getPath()));
-    }
-
-    setFilterContentTypes(contentTypes: api.schema.content.ContentType[]) {
-        this.getLoader().setFilterContentTypes(contentTypes);
     }
 
     clearCombobox() {
         super.clearCombobox();
         this.getComboBox().getComboBoxDropdownGrid().removeAllOptions();
-        this.getLoader().resetSearchString();
     }
 
     getSelectedDisplayValues(): ContentAndStatusTreeSelectorItem[] {
@@ -98,9 +85,5 @@ class MoveReadOnlyChecker {
 
     setFilterContentPaths(contentPaths: ContentPath[]) {
         this.filterContentPaths = contentPaths;
-    }
-
-    setFilterContentTypes(contentTypes: ContentTypeName[]) {
-        this.filterContentTypes = contentTypes;
     }
 }
