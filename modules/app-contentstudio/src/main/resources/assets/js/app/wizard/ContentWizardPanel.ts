3878b90f76619d1ae35becd6a66e61dfcd9a6907
Pavel Milkevich <pavel.milkevich@gmail.com>
2017-10-02 23:09:41 +0300

    Shortcut content - error when switching between versions #94

--- modules/app-contentstudio/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
+++ modules/app-contentstudio/src/main/resources/assets/js/app/wizard/ContentWizardPanel.ts
@@ -829,7 +829,7 @@ export class ContentWizardPanel
                 });
             }
 
-            if (this.getPersistedItem().getContentId().equals(contentId)) {
+            if (this.getPersistedItem().getContentId().equals(contentId) && this.contentType.isSite()) {
                 new ContentWizardDataLoader().loadSite(contentId).then(site => {
                     this.siteModel.update(site);
                 }).catch(api.DefaultErrorHandler.handle).done();
