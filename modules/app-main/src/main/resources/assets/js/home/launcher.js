2b745ca35343385ce72ebfe3503e775bfa879802
Pavel Milkevich <pavel.milkevich@gmail.com>
2017-10-17 18:31:43 +0300

    ESC button doesn't close current modal on Home screen first #36
    - fix for all the rest key bindings

--- modules/app-main/src/main/resources/assets/js/home/launcher.js
+++ modules/app-main/src/main/resources/assets/js/home/launcher.js
@@ -187,27 +187,54 @@ function closeLauncherPanel(skipTransition) {
     unselectCurrentApp();
 }
 
-var closeBinding = new api.ui.KeyBinding('esc').setGlobal(true).setCallback(function(e , combo) {
+var closeLauncher = new api.ui.KeyBinding('esc').setGlobal(true).setCallback(function(e , combo) {
     if (!isPanelExpanded()) {
         return;
     }
-    // preventing Browser shortcuts to kick in
-    if (e.preventDefault) {
-        e.preventDefault();
-    } else {
-        // internet explorer
-        e.returnValue = false;
-    }
+    e.preventDefault();
+    e.returnValue = false;
+
     closeLauncherPanel();
     return false;
 });
+var prevApp = new api.ui.KeyBinding('up').setGlobal(true).setCallback(function(e, combo) {
+    if (!isPanelExpanded()) {
+        return;
+    }
+
+    initKeyboardNavigation();
+    selectPreviousApp();
+});
+var nextApp = new api.ui.KeyBinding('down').setGlobal(true).setCallback(function(e, combo) {
+    if (!isPanelExpanded()) {
+        return;
+    }
+    e.preventDefault();
+    e.returnValue = false;
+
+    initKeyboardNavigation();
+    selectNextApp();
+});
+var runApp = new api.ui.KeyBinding('enter').setGlobal(true).setCallback(function(e, combo) {
+    if (!isPanelExpanded()) {
+        return;
+    }
+    e.preventDefault();
+    e.returnValue = false;
+
+    var selectedApp = getSelectedApp();
+    if (selectedApp) {
+        startApp(selectedApp);
+    }
+});
+var launcherBindings = [closeLauncher, prevApp, nextApp, runApp];
 
 function listenToKeyboardEvents() {
-    api.ui.KeyBindings.get().bindKey(closeBinding);
+    api.ui.KeyBindings.get().bindKeys(launcherBindings);
 }
 
 function unlistenToKeyboardEvents() {
-    api.ui.KeyBindings.get().unbindKey(closeBinding);
+    api.ui.KeyBindings.get().unbindKeys(launcherBindings);
 }
 
 function getSelectedApp() {
@@ -277,6 +304,106 @@ function initApplicationsListeners() {
         return true;
     }
     return false;
+}
+
+var launcherMainContainer;
+
+function listenToMouseMove() {
+    window.addEventListener("mousemove", disableKeyboardNavigation, true);
+}
+
+function disableKeyboardNavigation() {
+    getLauncherMainContainer().classList.remove("keyboard-navigation");
+    unselectCurrentApp();
+    window.removeEventListener("mousemove", disableKeyboardNavigation, true);
+}
+
+function initKeyboardNavigation() {
+    var appContainer = getLauncherMainContainer();
+    if (!appContainer.classList.contains("keyboard-navigation")) {
+        listenToMouseMove();
+        appContainer.classList.add("keyboard-navigation");
+    }
+}
+
+function getSelectedApp() {
+    return getLauncherMainContainer().querySelector('.app-row.selected');
+}
+
+function getSelectedAppIndex() {
+    var apps = getLauncherMainContainer().querySelectorAll('.app-row');
+    for (var i = 0; i < apps.length; i++) {
+        if (apps[i].classList.contains("selected")) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function selectNextApp() {
+    var firstAppIndex = isHomeAppActive() ? 1 : 0;
+    var selectedIndex = getSelectedAppIndex();
+    var apps = getLauncherMainContainer().querySelectorAll('.app-row');
+
+    selectApp((selectedIndex + 1) === apps.length || selectedIndex === -1 ? firstAppIndex : selectedIndex + 1);
+}
+
+function selectPreviousApp() {
+    var selectedIndex = getSelectedAppIndex();
+    var nextIndex;
+    if (selectedIndex === -1) {
+        nextIndex = isHomeAppActive() ? 1 : 0;
+    }
+    else if (selectedIndex === 0 || (selectedIndex === 1 && isHomeAppActive())) {
+        nextIndex = document.querySelectorAll('.app-row').length - 1;
+    }
+    else {
+        nextIndex = selectedIndex - 1;
+    }
+
+    selectApp(nextIndex);
+}
+
+function selectApp(index) {
+    unselectCurrentApp();
+    getAppByIndex(index).classList.add("selected");
+}
+
+function unselectCurrentApp() {
+    var selectedApp = getSelectedApp();
+    if (selectedApp) {
+        selectedApp.classList.remove("selected");
+    }
+}
+
+function getAppByIndex(index) {
+    var apps = getLauncherMainContainer().querySelectorAll('.app-row');
+    for (var i = 0; i < apps.length; i++) {
+        if (i === index) {
+            return apps[i];
+        }
+    }
+    return null;
+}
+
+function startApp(app) {
+    var anchorEl = app.parentElement;
+    if (anchorEl && anchorEl.tagName === 'A' && anchorEl.click) {
+        unselectCurrentApp();
+        anchorEl.click();
+    }
+}
+
+function getLauncherMainContainer() {
+    if (!launcherMainContainer) {
+        launcherMainContainer = document.querySelector('.launcher-main-container');
+    }
+
+    return launcherMainContainer;
+}
+
+function isHomeAppActive() {
+    return getLauncherMainContainer().classList.contains("home");
 }
 
 exports.init = function () {
