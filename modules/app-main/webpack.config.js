2ac0233e126aa35ccc7fe68218eb12a76dad9f07
edloidas <edloidas@gmail.com>
2017-10-22 21:31:49 +0300

    Upgrade core packages and fix build problems #169
    Moved PostCSS config to the root folder to use it from several apps in future.
    Added `browserslist-config-enonic` shared config.
    Updated `package.json` and `package-lock.json` to support the latest version of Browserslist package in `postcss-normalize` and `postcss-autoprefixer`, that support shared configs.

--- modules/app-main/webpack.config.js
+++ modules/app-main/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     context: `${__dirname}/src/main/resources/assets`,
     entry: {
         home: './js/home/main.js',
-        launcher: './js/launcher/main.js',
+        launcher: './js/launcher/main.js'
     },
     output: {
         path: `${__dirname}/build/resources/main/assets/`,
@@ -19,7 +19,7 @@ module.exports = {
                     fallback: 'style-loader',
                     use: [
                         { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
-                        { loader: 'postcss-loader', options: { sourceMap: true } },
+                        {loader: 'postcss-loader', options: {sourceMap: true, config: {path: '../../postcss.config.js'}}},
                         { loader: 'less-loader', options: { sourceMap: true } }
                     ]
                 })
