10e0abf8238e6566ab0886c14f1332a0b47e213a
bradnitski <vbr@enonic.com>
2017-12-07 13:52:33 +0300

    Changes to Info section #278

--- modules/app-applications/src/main/resources/assets/js/app/view/ApplicationItemStatisticsPanel.ts
+++ modules/app-applications/src/main/resources/assets/js/app/view/ApplicationItemStatisticsPanel.ts
@@ -14,7 +14,8 @@ import MacroDescriptor = api.macro.MacroDescriptor;
 import i18n = api.util.i18n;
 import DivEl = api.dom.DivEl;
 
-export class ApplicationItemStatisticsPanel extends api.app.view.ItemStatisticsPanel<api.application.Application> {
+export class ApplicationItemStatisticsPanel
+    extends api.app.view.ItemStatisticsPanel<api.application.Application> {
 
     private applicationDataContainer: api.dom.DivEl;
     private actionMenu: api.ui.menu.ActionMenu;
@@ -65,13 +66,16 @@ export class ApplicationItemStatisticsPanel extends api.app.view.ItemStatisticsP
 
         this.applicationDataContainer.removeChildren();
 
-        const infoGroup = new ItemDataGroup(i18n('field.info'), 'info');
+        const infoGroup = new ItemDataGroup(i18n('field.application'), 'application');
         const minVersion = currentApplication.getMinSystemVersion();
-        const maxVersion = currentApplication.getMaxSystemVersion();
-        infoGroup.addDataList(i18n('field.buildDate'), 'TBA');
+        const modifiedTime = currentApplication.getModifiedTime();
+
+        if (modifiedTime) {
+            infoGroup.addDataList(i18n('field.installed'), modifiedTime.toDateString() + ' ' + modifiedTime.toLocaleTimeString());
+        }
         infoGroup.addDataList(i18n('field.version'), currentApplication.getVersion());
         infoGroup.addDataList(i18n('field.key'), currentApplication.getApplicationKey().toString());
-        infoGroup.addDataList(i18n('field.systemRequired'), i18n('field.systemRequired.value', minVersion, maxVersion));
+        infoGroup.addDataList(i18n('field.systemRequired'), i18n('field.systemRequired.value', minVersion));
 
         let descriptorResponse = this.initDescriptors(currentApplication.getApplicationKey());
         let schemaResponse = this.initSchemas(currentApplication.getApplicationKey());
@@ -108,12 +112,11 @@ export class ApplicationItemStatisticsPanel extends api.app.view.ItemStatisticsP
 
         let macroPromises = [macroRequest.sendAndParse()];
 
-        return wemQ.all(macroPromises).spread((macros: MacroDescriptor[])=> {
+        return wemQ.all(macroPromises).spread((macros: MacroDescriptor[]) => {
 
             let macrosGroup = new ItemDataGroup(i18n('field.macros'), 'macros');
 
-            let macroNames = macros.
-            filter((macro: MacroDescriptor) => {
+            let macroNames = macros.filter((macro: MacroDescriptor) => {
                 return !ApplicationKey.SYSTEM.equals(macro.getKey().getApplicationKey());
             }).map((macro: MacroDescriptor) => {
                 return macro.getDisplayName();
