de88c2bb66942be775a7b36622712d39f3c595e4
Glenn Ricaud <gri@enonic.com>
2018-01-05 13:24:39 +0100

    XP Apps - Testing - Applications - Grid Panel & Toolbar (#438)

--- testing/app-applications/specs/app.browse.panel.toolbar.spec.js
+++ testing/app-applications/specs/app.browse.panel.toolbar.spec.js
@@ -3,20 +3,30 @@ const expect = chai.expect;
 const assert = chai.assert;
 var webDriverHelper = require('../libs/WebDriverHelper');
 const appBrowsePanel = require('../page_objects/applications/applications.browse.panel');
+const installAppDialog = require('../page_objects/applications/install.app.dialog');
 const studioUtils = require('../libs/studio.utils.js');
 
 describe('Application Browse Panel,  check buttons on the toolbar', function () {
     this.timeout(70000);
     webDriverHelper.setupBrowser();
 
+    const appDisplayName1 = 'Content viewer';
+    const appDisplayName2 = 'ContentHive';
+    const appDescription1 = 'Inspect your content';
+    const appDescription2 = 'Widget generating';
 
-    it(`test 1 description....`, () => {
-
+    it('WHEN Install button has been clicked for two applications THEN two rows should be present in the grid', () => {
+        return appBrowsePanel.clickOnInstallButton()
+            .then(() => Promise.all(
+                [installAppDialog.waitForInstallLink(appDisplayName1), installAppDialog.waitForInstallLink(appDisplayName2)]))
+            .then(() => Promise.all(
+                [installAppDialog.clickOnInstallAppLink(appDisplayName1), installAppDialog.clickOnInstallAppLink(appDisplayName2)]))
+            .then(() => Promise.all(
+                [installAppDialog.waitForAppInstalled(appDisplayName1), installAppDialog.waitForAppInstalled(appDisplayName2)]))
+            .then(() => installAppDialog.clickOnCancelButtonTop)
+            .then(() => installAppDialog.waitForClosed)
+            .then(() => Promise.all([appBrowsePanel.isItemDisplayed(appDescription1), appBrowsePanel.isItemDisplayed(appDescription2)]));
     });
-    it(`test 2 description...`, () => {
-
-    });
-
 
     beforeEach(() => studioUtils.navigateToApplicationsApp(webDriverHelper.browser));
     afterEach(() => studioUtils.doCloseCurrentBrowserTab(webDriverHelper.browser));
