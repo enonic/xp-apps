29dfe52975cef2cb83311fad1846ccfa2f6437fa
sig <sig@enonic.com>
2018-01-24 14:46:31 +0300

    catch err in tests

--- testing/app-contentstudio/page_objects/browsepanel/content.browse.panel.js
+++ testing/app-contentstudio/page_objects/browsepanel/content.browse.panel.js
@@ -139,29 +139,44 @@ var contentBrowsePanel = Object.create(page, {
     },
     waitForNewButtonEnabled: {
         value: function () {
-            return this.waitForEnabled(this.newButton, 3000);
+            return this.waitForEnabled(this.newButton, 3000).catch(err=> {
+                this.saveScreenshot('err_new_button');
+                throw Error('New button is not enabled after ' + 3000 + 'ms')
+            })
         }
     },
 
     waitForEditButtonEnabled: {
         value: function () {
-            return this.waitForEnabled(this.editButton, 3000);
+            return this.waitForEnabled(this.editButton, 3000).catch(err=> {
+                this.saveScreenshot('err_edit_button');
+                throw Error('Edit button is not enabled after ' + 3000 + 'ms')
+            })
         }
     },
     waitForDeleteButtonEnabled: {
         value: function () {
-            return this.waitForEnabled(this.deleteButton, 3000);
+            return this.waitForEnabled(this.deleteButton, 3000).catch(err=> {
+                this.saveScreenshot('err_delete_button');
+                throw Error('Delete button is not enabled after ' + 3000 + 'ms')
+            })
         }
     },
     waitForDeleteButtonDisabled: {
         value: function () {
-            return this.waitForDisabled(this.deleteButton, 3000);
+            return this.waitForDisabled(this.deleteButton, 3000).catch(err=> {
+                this.saveScreenshot('err_delete_disabled_button');
+                throw Error('Delete button should be disabled, timeout: ' + 3000 + 'ms')
+            })
         }
     },
 
     isDeleteButtonEnabled: {
         value: function () {
-            return this.isEnabled(this.deleteButton);
+            return this.isEnabled(this.deleteButton).catch(err=> {
+                this.saveScreenshot('err_delete_button');
+                throw Error('Delete button should be enabled, timeout ' + 3000 + 'ms')
+            })
         }
     },
     isNewButtonEnabled: {
