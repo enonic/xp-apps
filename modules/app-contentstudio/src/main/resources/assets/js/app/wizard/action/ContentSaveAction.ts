b9d887254590e8f6af56d454cf349a3998c6d68c
Bradnitski <vbr@enonic.com>
2018-02-05 10:27:40 +0100

    Incorrect label of Save button after content form was changed and then reset to its original state #503

--- modules/app-contentstudio/src/main/resources/assets/js/app/wizard/action/ContentSaveAction.ts
+++ modules/app-contentstudio/src/main/resources/assets/js/app/wizard/action/ContentSaveAction.ts
@@ -1,20 +1,37 @@
 import i18n = api.util.i18n;
-import {ContentWizardPanel} from '../ContentWizardPanel';
 import SaveAction = api.app.wizard.SaveAction;
+import {ContentWizardPanel} from '../ContentWizardPanel';
 
 export class ContentSaveAction
     extends SaveAction {
+
+    private static SAVED_STATE: string = 'saved';
 
     constructor(wizardPanel: ContentWizardPanel, label: string = i18n('action.save')) {
         super(wizardPanel, label);
 
         this.setEnabled(false);
     }
+
+    isSavedStateEnabled(): boolean {
+        return ContentSaveAction.SAVED_STATE == this.getIconClass();
+    }
+
     protected saveChanges(wizardPanel: ContentWizardPanel): wemQ.Promise<any> {
         this.setLabel(i18n('action.saving'));
 
-        return wizardPanel.saveChanges().
-        catch((reason: any) => api.DefaultErrorHandler.handle(reason)).
-        finally(() => this.setLabel(i18n('action.saved')));
+        return wizardPanel.saveChanges().then(() => {
+            this.setLabel(i18n('action.saved'));
+
+            this.toggleIconClass(ContentSaveAction.SAVED_STATE, true);
+
+        }, (reason: any) => {
+            this.setLabel(i18n('action.save'));
+            this.setEnabled(true);
+
+            this.toggleIconClass(ContentSaveAction.SAVED_STATE, false);
+
+            api.DefaultErrorHandler.handle(reason);
+        });
     }
 }
