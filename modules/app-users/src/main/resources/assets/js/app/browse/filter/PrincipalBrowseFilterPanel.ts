5011dd6a0ac2d01f74d657b9470c01f4c4a3f479
Alan Semenov <ase@enonic.com>
2018-02-28 13:12:16 +0100

    Implemented alphabetical sorting of principal types inside the User app's Search panel. Refactoring (#544)

--- modules/app-users/src/main/resources/assets/js/app/browse/filter/PrincipalBrowseFilterPanel.ts
+++ modules/app-users/src/main/resources/assets/js/app/browse/filter/PrincipalBrowseFilterPanel.ts
@@ -1,6 +1,5 @@
 import '../../../api.ts';
 import {UserTreeGridItem} from '../UserTreeGridItem';
-import {PrincipalTypeAggregationGroupView} from './PrincipalTypeAggregationGroupView';
 import {ListUserItemsRequest} from '../../../api/graphql/principal/ListUserItemsRequest';
 import {UserItemType} from '../UserItemType';
 import {PrincipalBrowseSearchData} from './PrincipalBrowseSearchData';
@@ -10,8 +9,10 @@ import BrowseFilterSearchEvent = api.app.browse.filter.BrowseFilterSearchEvent;
 import AggregationGroupView = api.aggregation.AggregationGroupView;
 import AggregationSelection = api.aggregation.AggregationSelection;
 import Aggregation = api.aggregation.Aggregation;
+import BucketAggregation = api.aggregation.BucketAggregation;
 import Bucket = api.aggregation.Bucket;
 import i18n = api.util.i18n;
+import StringHelper = api.util.StringHelper;
 
 export class PrincipalBrowseFilterPanel
     extends api.app.browse.filter.BrowseFilterPanel<UserTreeGridItem> {
@@ -19,24 +20,21 @@ export class PrincipalBrowseFilterPanel
     static PRINCIPAL_TYPE_AGGREGATION_NAME: string = 'principalType';
     static PRINCIPAL_TYPE_AGGREGATION_DISPLAY_NAME: string = i18n('field.types');
 
-    private principalTypeAggregation: PrincipalTypeAggregationGroupView;
+    private principalTypeAggregationGroupView: AggregationGroupView;
 
     constructor() {
         super();
 
-        this.initHitsCounter().then(() => this.initAggregationGroupView([this.principalTypeAggregation]));
+        this.initHitsCounter();
     }
 
     protected getGroupViews(): api.aggregation.AggregationGroupView[] {
-        this.principalTypeAggregation = new PrincipalTypeAggregationGroupView(
+        this.principalTypeAggregationGroupView = new AggregationGroupView(
             PrincipalBrowseFilterPanel.PRINCIPAL_TYPE_AGGREGATION_NAME,
-            PrincipalBrowseFilterPanel.PRINCIPAL_TYPE_AGGREGATION_DISPLAY_NAME);
+            PrincipalBrowseFilterPanel.PRINCIPAL_TYPE_AGGREGATION_DISPLAY_NAME
+        );
 
-        return [this.principalTypeAggregation];
-    }
-
-    private initAggregationGroupView(aggregationGroupViews: AggregationGroupView[]) {
-        aggregationGroupViews.forEach(aggregation => aggregation.initialize());
+        return [this.principalTypeAggregationGroupView];
     }
 
     doRefresh(): wemQ.Promise<void> {
@@ -111,6 +109,7 @@ export class PrincipalBrowseFilterPanel
                 if (fireEvent) {
                     new BrowseFilterSearchEvent(new PrincipalBrowseSearchData(searchString, types, userItems)).fire();
                 }
+
                 this.updateAggregations(result.aggregations, !!searchString);
                 this.updateHitsCounter(userItems ? userItems.length : 0, api.util.StringHelper.isBlank(searchString));
                 this.toggleAggregationsVisibility(result.aggregations);
@@ -125,15 +124,11 @@ export class PrincipalBrowseFilterPanel
 
     private toggleAggregationsVisibility(aggregations: Aggregation[]) {
         aggregations.forEach((aggregation: api.aggregation.BucketAggregation) => {
-            let aggregationIsEmpty = !aggregation.getBuckets().some((bucket: api.aggregation.Bucket) => {
-                if (bucket.docCount > 0) {
-                    return true;
-                }
-            });
+            const aggregationIsEmpty = !aggregation.getBuckets().some(bucket => bucket.docCount > 0);
 
             const isTypeAggregation = aggregation.getName() === PrincipalBrowseFilterPanel.PRINCIPAL_TYPE_AGGREGATION_NAME;
             if (isTypeAggregation) {
-                this.principalTypeAggregation.setVisible(!aggregationIsEmpty);
+                this.principalTypeAggregationGroupView.setVisible(!aggregationIsEmpty);
             }
         });
     }
@@ -147,6 +142,51 @@ export class PrincipalBrowseFilterPanel
             }).catch((reason: any) => {
                 api.DefaultErrorHandler.handle(reason);
             });
+    }
+
+    updateAggregations(aggregations: api.aggregation.BucketAggregation[], doUpdateAll?: boolean) {
+        this.initPrincipalTypeBuckets(aggregations);
+
+        super.updateAggregations(aggregations, doUpdateAll);
+
+    }
+
+    private getPrincipalTypeDisplayName(key: string) {
+        switch (key.toLowerCase()) {
+            case 'user': return i18n('field.user');
+            case 'group': return i18n('field.group');
+            case 'role': return i18n('field.role');
+            case 'user store': return i18n('field.userStore');
+            default: StringHelper.capitalize(name);
+        }
+    }
+
+    private initPrincipalTypeBuckets(aggregations: BucketAggregation[]) {
+        const principalTypeAggregation: BucketAggregation =
+            aggregations.filter(
+                aggregation => aggregation.getName() === PrincipalBrowseFilterPanel.PRINCIPAL_TYPE_AGGREGATION_NAME
+            )[0];
+
+        if (!principalTypeAggregation) {
+            return;
+        }
+
+        const buckets = principalTypeAggregation.getBuckets();
+        buckets.forEach(bucket => bucket.setDisplayName(this.getPrincipalTypeDisplayName(bucket.getKey())));
+
+        this.sortPrincipalTypeBuckets(principalTypeAggregation);
+
+    }
+
+    private sortPrincipalTypeBuckets(principalTypeAggregation: BucketAggregation) {
+        const sortedBuckets = principalTypeAggregation.getBuckets().sort((b1: Bucket, b2: Bucket) => {
+            const bucketName1 = this.getPrincipalTypeDisplayName(b1.getKey());
+            const bucketName2 = this.getPrincipalTypeDisplayName(b2.getKey());
+
+            return (bucketName1 < bucketName2) ? -1 : 1;
+        });
+
+        principalTypeAggregation.setBuckets(sortedBuckets);
     }
 
 }
