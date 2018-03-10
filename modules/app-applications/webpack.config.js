1e564863d975ca1a6a55be845fc3d5fc8d26aa26
Mikita Taukachou <edloidas@gmail.com>
2018-03-10 23:06:13 +0300

    app-applications: Remove dependency from lib-admin-ui styles #587
    Updated webpack config to compile styles.
    Upgraded dependencies.

--- modules/app-applications/webpack.config.js
+++ modules/app-applications/webpack.config.js
@@ -1,20 +1,70 @@
 const ErrorLoggerPlugin = require('error-logger-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const path = require('path');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    entry: './src/main/resources/assets/js/main.ts',
+    context: path.join(__dirname, '/src/main/resources/assets'),
+    entry: {
+        'js/bundle': './js/main.ts',
+        'styles/_all': './styles/main.less',
+    },
     output: {
-        filename: './build/resources/main/assets/js/bundle.js'
+        path: path.join(__dirname, '/build/resources/main/assets'),
+        filename: './[name].js'
     },
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js', '.less', '.css']
     },
     module: {
-        loaders: [
-            {test: /\.tsx?$/, loader: "ts-loader"}
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader"
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    publicPath: '../../',
+                    use: [
+                        {loader: 'css-loader', options: {sourceMap: !isProd, importLoaders: 1}},
+                        {loader: 'postcss-loader', options: {sourceMap: !isProd, config: {path: '../../postcss.config.js'}}},
+                        {loader: 'less-loader', options: {sourceMap: !isProd}}
+                    ]
+                })
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf)$|icomoon.svg/,
+                use: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /^((?!icomoon).)*\.(svg|png|jpg|gif)$/,
+                use: 'file-loader?name=img/[name].[ext]'
+            }
         ]
     },
     plugins: [
-        new ErrorLoggerPlugin()
+        new ErrorLoggerPlugin(),
+        new ExtractTextPlugin({
+            filename: '[name].css',
+            allChunks: true,
+            disable: false
+        }),
+        ...(isProd ? [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    mangle: false,
+                    keep_classnames: true,
+                    keep_fnames: true
+                }
+            })
+        ] : [])
     ],
-    devtool: 'source-map'
+    devtool: isProd ? false : 'source-map'
 };
